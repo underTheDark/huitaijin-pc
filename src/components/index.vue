79b211ae8628ede72b783004f9e19b12ae079072
joe <980846821@qq.com>
2019-06-10 18:08:20 +0800

    first commit

---
+++ src/components/index.vue
@@ -0,0 +1,239 @@
+<template>
+  <div id="index">
+    <div id="header">
+      <div class="header-left">
+        <img src="../../static/img/index/Logo.png" />
+        <span>河南汇台金集团</span>
+      </div>
+      <div class="header-right">
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+        >
+          <el-menu-item index="1">首页</el-menu-item>
+          
+          <el-menu-item index="2" >产品</el-menu-item>
+          <el-menu-item index="3" >关于我们</el-menu-item>
+          <el-menu-item index="4" >联系我们</el-menu-item>
+          
+        </el-menu>
+      </div>
+    </div>
+    <router-view></router-view>
+    <div id="footer">
+        <div class="footer-left">
+            <div>
+                 <img src="../../static/img/index/Logo.png" />
+                 <span>河南汇台金集团</span>
+            </div>
+             <div>河南汇台金集团是一家以动物营养科技、尖端生物科技为基础，严格按照兽药GMP标准建造，专业从事高端饲料添加剂科研、生产、销售于一体的科技型技术企业。</div>
+             <div class="partThree">
+                 <span>京公网安备&nbsp;11000002000088号</span>
+                 <span>营业执照</span>
+                 <span>经营执照</span>
+             </div>
+        </div>
+        <div class="footer-right">
+            <div>关于</div>
+            <div>
+                <span>公司名称：河南汇台金实业有限公司</span>
+                <span class="shugang"></span>
+                 <span>销售地址：花园路69号</span>
+                 <span class="shugang"></span>
+                  <span>生产地址：河南省郑州市花园口工业区</span>
+            </div>
+             <div>
+                <span>电话：0371-55150201</span>
+                <span class="shugang"></span>
+                 <span>传真：0371-55150201</span>
+                 <span class="shugang"></span>
+                  <span>邮编：45000</span>
+            </div>
+             <div>
+               <img src="../../static/img/index/qq.png" />
+                <img src="../../static/img/index/weibo.png" />
+                 <img src="../../static/img/index/weixin.png" />
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+         return{
+         activeIndex: '1',
+         }
+    },
+     methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+
+};
+</script>
+
+<style scoped>
+#index{
+height:2924px;
+ width:1440px;  
+ margin:0 auto;
+}
+/* 头部 */
+#header{
+
+height:72px;
+background:rgba(0,0,0,1);
+opacity:0.9;
+overflow: hidden;
+}
+#header .header-left{
+    float:left;
+    margin-left:130px;
+    height:72px;
+   
+}
+.header-left img{
+width:48px;
+height:48px;
+
+}
+.header-left span{
+display:inline-block;
+height:72px;
+line-height:72px;
+  width:126px;
+font-size:18px;
+font-family:PingFangSC-Medium;
+font-weight:500;
+color:rgba(0,78,30,1);
+
+}
+.header-right{
+    float:right;
+    height:72px;
+}
+.header-right .el-menu{
+height:100%;
+}
+.el-menu .el-menu-item{
+    height:72px;
+    line-height:72px;
+    margin:0 36px;
+    color:rgba(51,51,51,1);
+    padding:0;
+    
+
+}
+.el-menu .is-active{
+     color:rgba(0,61,34,1);
+    border-color:rgba(0,61,34,1)
+}
+
+
+/* 尾部 */
+#footer{
+    width:1440px;
+height:280px;
+background:linear-gradient(135deg,rgba(96,205,151,1) 0%,rgba(34,149,108,1) 100%);
+}
+.footer-left{
+    float:left;
+    margin-left:130px;
+    width:436px;
+}
+.footer-left div:nth-child(1){
+    overflow: hidden;
+    height:48px;
+    line-height:48px;
+    margin:56px 0 20px;
+}
+.footer-left  div:nth-child(1) img{
+    width:48px;
+    height:48px;
+    
+    border: none;
+    float:left;
+}
+.footer-left div:nth-child(1) span{
+    float:left;
+    display: inline-block;
+    width:112px;
+    height:48px; 
+line-height:48px;
+font-size:16px;
+font-family:PingFangSC-Semibold;
+font-weight:600;
+color:rgba(255,255,255,1);
+line-height:22px;
+}
+
+.footer-left div:nth-child(2){
+    width:436px;
+height:34px;
+font-size:12px;
+font-family:PingFangSC-Semibold;
+font-weight:500;
+color:white;
+}
+.footer-left div:nth-child(3){
+    margin-top:30px;
+    text-align: left;
+
+font-size:12px;
+font-family:PingFangSC-Semibold;
+font-weight:500;
+color:rgba(255,255,255,1);
+
+}
+.footer-left .partThree span{
+ border-right:1px solid white;
+ margin-right:0;
+ padding:0 10px;
+}
+.footer-left .partThree span:nth-child(3){
+ border-right:none;
+}
+.footer-left .partThree span:nth-child(1){
+ padding-left:0;
+
+}
+
+/* 尾部右部分 */
+.footer-right{
+    float:left;
+    margin-left:80px;
+    text-align: left;
+}
+.footer-right div:nth-child(1){
+margin:56px 0 20px 0;
+ height:48px;
+font-size:16px;
+font-family:PingFangSC-Semibold;
+font-weight:600;
+color:rgba(255,255,255,1);
+line-height:48px;
+}
+
+.footer-right{
+    height:20px;
+font-size:14px;
+font-family:PingFangSC-Semibold;
+
+color:rgba(255,255,255,1);
+line-height:20px;
+}
+.footer-right .shugang{
+    border-right:1px solid white;
+    margin:0 10px;
+}
+.footer-right div:nth-child(3){
+    margin:10px 0;
+}
+</style>
+
+
